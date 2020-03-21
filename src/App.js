cf48a9e576a35fd1bad4ce806f0c57cf50994ef0
jharrel <harrel.jason@gmail.com>
2020-03-21 15:01:15 -0700

    Updated search bar

--- src/App.js
+++ src/App.js
@@ -7,16 +7,34 @@ import {SearchBar, VideoDetail, VideoList } from './components';
 import youtube from './api/youtube';
 
 class App extends React.Component {
+    state = {
+        video: [],
+        selectedVideo: null,
+    }
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', { 
+            params: {
+                part: 'snippet',
+                maxResults: 5,
+                key: 'AIzaSyBfr7HTDfXgPV-bVcH6ZSEWYeK4gJgW9hU',
+                q: searchTerm,
+            }
+        });
+
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
+    }
+
     render (){
+        const { selectedVideo } = this.state;
         return(
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs={12}>
-                            <SearchBar />
+                            <SearchBar onFormSubmit={this.handleSubmit} />
                         </Grid>
                         <Grid item xs={8}>
-                            <VideoDetail />
+                            <VideoDetail video={selectedVideo} />
                         </Grid>
                         <Grid item xs={4}>
                             {/* <VideoList /> */}
--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Paper, Typography } from '@material-ui/core';
+
 const VideoDetail = () => {
     return (
         <h1>This is a video Detail component</h1>
